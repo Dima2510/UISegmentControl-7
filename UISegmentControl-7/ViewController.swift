c3de4b0bf933ff859b6bd2c7ae2534907e7b04a4
Dima <protsack.dima25@gmail.com>
2022-07-23 01:16:02 +0300

    Initial Commit

---
+++ UISegmentControl-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentControl-7
+//
+//  Created by Дмитрий Процак on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
